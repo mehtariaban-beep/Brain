db7c890f74d11b8f664be011d6abbcaf29484856
mehtariaban-beep <mehtariaban@gmail.com>
2025-09-02 07:24:37 +0330

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,1 @@
+const CACHE_NAME="brainfog-cache-v1";const urlsToCache=["/index.html","/manifest.json","/brainfog-app-iphone-13.zip"];self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)));});self.addEventListener("fetch",event=>{event.respondWith(caches.match(event.request).then(resp=>resp||fetch(event.request)));});
